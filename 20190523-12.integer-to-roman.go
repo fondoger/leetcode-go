066c56cdbb37622f0f4f690f16497169bfaccb14
fondoger <fondoger@outlook.com>
2019-05-23 18:28:11 +0800

    rewrite #12 using two arrays

--- 20190523-12.integer-to-roman.go
+++ 20190523-12.integer-to-roman.go
@@ -8,25 +8,11 @@ package main
 /* Solution 1: using map */
 func intToRoman(num int) string {
 	res := ""
-	mapping := map[int]string{
-		1000: "M",
-		900:  "CM",
-		500:  "D",
-		400:  "CD",
-		100:  "C",
-		90:   "XC",
-		50:   "L",
-		40:   "XL",
-		10:   "X",
-		9:    "IX",
-		5:    "V",
-		4:    "IV",
-		1:    "I",
-	}
 	keys := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	for _, key := range keys {
+	values := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	for i, key := range keys {
 		for num >= key {
-			res += mapping[key]
+			res += values[i]
 			num -= key
 		}
 	}
