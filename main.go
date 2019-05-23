4e907014c1b59d0cbb34b13b6973507386144c3c
fondoger <fondoger@outlook.com>
2019-05-23 10:32:53 +0800

    测试golang字符串类型

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+import "reflect"
+
 func main() {
-	// 测试切片
-	print(convert("PAYPALISHIRING", 3))
+	// 测试字符串索引字符类型
+	s := "Hello, World"
+	s2 := "你好，世界1234567"
+	println((reflect.TypeOf(s[0])).String())
+	println((reflect.TypeOf(s2[0])).String())
+	println(len(s2))        // 计算字节数
+	println(s2[:5])         // 依照byte截取
+	println(string(s2[20])) // 依照byte索引
 }
