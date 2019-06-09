1cf5bbbc6562a5ca82d34a723bf925a76b5c4536
fondoger <fondoger@outlook.com>
2019-06-09 23:27:35 +0800

    #142 add solution 2(巧妙的龟兔赛跑办法)

--- 20190609-142.linked-list-cycle-ii.go
+++ 20190609-142.linked-list-cycle-ii.go
@@ -13,8 +13,41 @@ package main
  * }
  */
 
-// Solution 1: 思路简单的办法
+// Solution 2: 巧妙的办法
+// 在解法1的基础上，我们可以证明：从起点到第一次相遇的点之间的窗口大小恰好是环的大小的整数倍
+// 这样就免去了计算环大小和构建滑动窗口的步骤
+// 证明如下：
+// 		设链表由长度为 p 的非环路径和长度为 C 的环路径组成。
+//	 	设在第一次相遇时，相遇点到环起点的距离为 m
+// 		  s_slow = p + m + i*C
+//		2*s_slow = p + m + j*C = s_fast
+//	 相减得s_slow = (j - i) * C，即双方同时到达相遇点时，乌龟走的路径长度恰好是环长度的整数倍，
+// 	 	因此，当乌龟第一次经过相遇点时，此时已经走了环长度的整数倍。
+/// 这个证明不完善，不要看！！！
 func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	hasCycle := false
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			hasCycle = true
+			break
+		}
+	}
+	if !hasCycle {
+		return nil // 无环
+	}
+	slow = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
+}
+
+// Solution 1: 思路简单的办法
+func detectCycle_1(head *ListNode) *ListNode {
 	// Step 1: 判定是否有环
 	slow, fast := head, head
 	hasCircle := false
@@ -36,7 +69,7 @@ func detectCycle(head *ListNode) *ListNode {
 		fast = fast.Next
 		count++
 	}
-	// Step 3: 维护一个大小为k的双指针窗口，不停向前移动，当指针首次相交时，交点即为环
+	// Step 3: 维护一个大小为count的双指针窗口，不停向前移动，当指针首次相交时，交点即为环
 	slow, fast = head, head
 	for i := 0; i < count; i++ {
 		fast = fast.Next
