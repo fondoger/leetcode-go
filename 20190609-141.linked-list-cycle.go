e5faa79c5952819f4accba2b0b1cf940f4652451
fondoger <fondoger@outlook.com>
2019-06-09 22:26:20 +0800

    #141 链表判断是否有环(龟兔赛跑算法)

---
+++ 20190609-141.linked-list-cycle.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+ * @lc app=leetcode id=141 lang=golang
+ *
+ * [141] Linked List Cycle
+ */
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if fast == slow {
+			return true
+		}
+	}
+	return false
+}
