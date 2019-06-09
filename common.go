cdecf745b444920c4885aeae1e1e15a13b7f0b1a
fondoger <fondoger@outlook.com>
2019-06-09 21:57:16 +0800

    添加二叉树的序列化与反序列化工具函数

--- common.go
+++ common.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"strconv"
+	"strings"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -34,4 +39,96 @@ func printIntArray(arr []int) {
 		print(item, " ")
 	}
 	println()
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 输入二叉树：
+//			0
+//		  /	  \
+//		 -3	   9
+//		/	  /
+//	  -10    5
+// 输出层序遍历结果:[0,-3,9,-10,null,5]
+// 假设树是一个满二叉树，层序遍历
+func serializeBinaryTree(root *TreeNode) string {
+	if root == nil {
+		return "[]"
+	}
+	res := "[" + strconv.Itoa(root.Val)
+	queue := make([]*TreeNode, 0, 10)
+	queue = append(queue, root.Left, root.Right)
+	nilCount := 0
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		if node == nil {
+			nilCount++
+		} else {
+			for nilCount > 0 {
+				res += ",null"
+				nilCount--
+			}
+			res += "," + strconv.Itoa(node.Val)
+			queue = append(queue, node.Left, node.Right)
+		}
+	}
+	res += "]"
+	return res
+}
+
+func strToTreeNode(str string) *TreeNode {
+	if str == "null" {
+		return nil
+	}
+	value, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return &TreeNode{value, nil, nil}
+}
+
+func deserializeBinaryTree(str string) *TreeNode {
+	str = str[1 : len(str)-1] // 去除[]中括号
+	items := strings.Split(str, ",")
+	root := strToTreeNode(items[0])
+	if root == nil {
+		return nil
+	}
+	items = items[1:]
+	queue := make([]*TreeNode, 0, 10)
+	queue = append(queue, root)
+	for len(queue) > 0 && len(items) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		if len(items) > 0 {
+			node.Left = strToTreeNode(items[0])
+			items = items[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+		}
+		if len(items) > 0 {
+			node.Right = strToTreeNode(items[0])
+			items = items[1:]
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+	}
+	return root
+}
+
+// 从LeetCode形式的二叉树结点数组构建二叉树
+func buildBinaryTree(str string) *TreeNode {
+	return deserializeBinaryTree(str)
+}
+
+// 按LeetCode方式层序打印二叉树
+func printBinaryTree(root *TreeNode) {
+	println(serializeBinaryTree(root))
 }
--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 func main() {
-	{
-		list1 := []int{1, 2, 3, 4, 5}
-		l1 := buildLinkedList(list1)
-		printLinkedList(l1)
-		printLinkedList(reverseBetween(l1, 2, 4))
-	}
+	t := "[5,4,7,3,null,2,null,-1,null,9]"
+
+	tree := buildBinaryTree(t)
+	printBinaryTree(tree)
+
+	root := &TreeNode{5, nil, nil}
+	root.Left = &TreeNode{4, nil, nil}
+	root.Left.Left = &TreeNode{3, nil, nil}
+	root.Left.Left.Left = &TreeNode{-1, nil, nil}
+	root.Right = &TreeNode{7, nil, nil}
+	root.Right.Left = &TreeNode{2, nil, nil}
+	root.Right.Left.Left = &TreeNode{9, nil, nil}
+	printBinaryTree(root)
 }
