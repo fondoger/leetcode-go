28dc4f52cec770e63867b5a04d9e794d66a1ac4d
fondoger <fondoger@outlook.com>
2019-06-10 23:57:50 +0800

    adjust filename

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
 func main() {
-	l1 := buildLinkedList("[1,2,2,1]")
-	l2 := buildLinkedList("[]")
-	l3 := buildLinkedList("[1]")
-	l4 := buildLinkedList("[1,2,3,4,5,4,3,2,1]")
-	println(isPalindrome(l1))
-	println(isPalindrome(l2))
-	println(isPalindrome(l3))
-	println(isPalindrome(l4))
+
 }
