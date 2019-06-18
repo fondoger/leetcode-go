2bfed3f941ca2af9cc3fc98dd21316d682e9ac5d
fondoger <fondoger@outlook.com>
2019-06-18 22:11:04 +0800

    #47 有重复数组的排列数(交换法)

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 func main() {
-	arr := buildIntArray("[1,2,3]")
-	for _, arr := range(permute(arr)) {
-		printIntArray(arr)
+	if true {
+		arr := buildIntArray("[2,1,2]")
+		for _, arr := range permuteUnique(arr) {
+			printIntArray(arr)
+		}
 	}
 }
