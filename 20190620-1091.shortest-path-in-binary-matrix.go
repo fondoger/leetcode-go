c3ef0983621957d03612157751a4c7977d5eb890
fondoger <fondoger@outlook.com>
2019-06-20 01:15:34 +0800

    #1091 无权最短路径：类似层序遍历的BFS

---
+++ 20190620-1091.shortest-path-in-binary-matrix.go
@@ -0,0 +1,80 @@
+package main
+
+/**
+ *	LeetCode 第141次周赛 第三题
+ *
+ * 对于单源最短路径问题：
+ * 容易理解的算法有：BFS算法、Dijkstra算法
+ * 其他算法有：自己百度去，反正我也学不会
+ *
+ * 一般在做算法题是，BFS算法是比较容易实现的
+ *
+ * 需要注意的是：当点与点之间的权值全部相等时，直接使用队列进行BFS即可，
+ * 特别简单，类似二叉树的层序遍历。此时普通BFS与Dijkstra效率一样。(注意1. 结点不要重复加入 2. 队列是类似带层次层序遍历分批读取)
+ * 如果权值不相等，也可以用队列，不过一个结点可能被加入队列多次，
+ * 即当发现到达这个结点的长度能更优时，此时效率逊于Dijkstra
+ */
+
+var Dir = [8][2]int{
+	{1, -1}, {1, 0}, {1, 1},
+	{0, 1}, {0, -1},
+	{-1, 1}, {-1, 0}, {-1, 1},
+}
+
+// 方法二：可重复的队列BFS解法，适用于有权最短路径问题
+func shortestPathBinaryMatrix(grid [][]int) int {
+	m, n := len(grid), len(grid[0])
+	visit := make([][]int, m+n)
+	queue := make(Queue47, 0)
+	queue.offer(Point47{0, 0})
+	visit[0][0] = 0
+
+}
+
+// 方法一：不重复加入的队列BFS解法：适用于权值相等的最短路径问题，用类似带层次的层序遍历分批处理
+func shortestPathBinaryMatrix_1(grid [][]int) int {
+	if grid[0][0] == 1 {
+		return -1
+	}
+	m, n := len(grid), len(grid[0])
+	queue := make(Queue47, 0, m+n)
+	queue.offer(Point47{0, 0})
+	res := 0
+	for !queue.isEmpty() {
+		res++ // res依批次递增
+		size := len(queue)
+		for i := 0; i < size; i++ { // 注意点1：分批读取
+			cur := queue.poll()
+			if cur.x == m-1 && cur.y == n-1 {
+				return res
+			}
+			for i := 0; i < 8; i++ {
+				next := Point47{cur.x + Dir[i][0], cur.y + Dir[i][1]}
+				if next.x >= 0 && next.x < m && next.y >= 0 && next.y < n && grid[next.x][next.y] == 0 {
+					println("offer", next.x, next.y)
+					queue.offer(next)
+					grid[next.x][next.y] = 1 // 注意点2：
+				}
+			}
+		}
+	}
+	return -1
+}
+
+/* A simple Queue implementation */
+type Point47 struct {
+	x, y int
+}
+type Queue47 []Point47
+
+func (q *Queue47) offer(val Point47) {
+	*q = append(*q, val)
+}
+func (q *Queue47) poll() Point47 {
+	res := (*q)[0]
+	*q = (*q)[1:]
+	return res
+}
+func (q *Queue47) isEmpty() bool {
+	return len(*q) == 0
+}
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 func main() {
 	if true {
-		arr := buildIntArray("[1,0,2,3,0,4,5,0]")
-		duplicateZeros(arr)
-		printIntArray(arr)
+		arr := [][]int{
+			{1, 0, 0},
+			{1, 1, 0},
+			{1, 1, 0},
+		}
+		// arr := make([][]int, 3)
+		// arr[0] = []int{0, 0, 0} // slice
+		// arr[1] = []int{1, 1, 0} // slice
+
+		println(shortestPathBinaryMatrix(arr))
 	}
 }
