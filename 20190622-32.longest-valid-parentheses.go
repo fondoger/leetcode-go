36eb6bdaf0d13b7d336f2db47d567d7fa7f67c8f
fondoger <fondoger@outlook.com>
2019-06-22 16:42:33 +0800

    #32 最长匹配括号 解法1：栈(存放下标)  解法2：动态规划

--- 20190622-32.longest-valid-parentheses.go
+++ 20190622-32.longest-valid-parentheses.go
@@ -5,7 +5,50 @@ package main
  *
  * [32] Longest Valid Parentheses
  */
+
+/*
+	最长括号匹配
+
+	主要有两种解法：
+	1. （简单的）栈解法，关键在于存放下标，而非存放左括号
+	2. （稍复杂的）动态规划解法
+
+*/
+
+// 解法2：动态规划
+// 数组dp[i]存放以 i 为结尾的最长括号匹配
 func longestValidParentheses(s string) int {
+	if len(s) < 2 {
+		return 0
+	}
+	dp := make([]int, len(s))
+	if s[0] == '(' && s[1] == ')' {
+		dp[1] = 2
+	}
+	res := dp[1]
+	for i := 2; i < len(s); i++ {
+		if s[i] == ')' {
+			if s[i-1] == '(' {
+				dp[i] = dp[i-2] + 2
+			} else if dp[i-1] > 0 { // s[i-1]必为右括号
+				preIdx := i - dp[i-1] - 1
+				if preIdx >= 0 && s[preIdx] == '(' {
+					dp[i] = dp[i-1] + 2
+					if preIdx-1 >= 0 {
+						dp[i] += dp[preIdx-1]
+					}
+				}
+			}
+			if dp[i] > res {
+				res = dp[i]
+			}
+		}
+	}
+	return res
+}
+
+// 解法1：使用栈，关键在存放下标而非左括号
+func longestValidParentheses_Using_Stack(s string) int {
 	stack := make(Stack32, 0)
 	res := 0
 	stack.push(-1)
