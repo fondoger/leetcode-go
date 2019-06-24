1f2d428ff0244f32c376bbd9523f1a32f527c6f2
fondoger <fondoger@outlook.com>
2019-06-25 00:07:12 +0800

    #100 判断二叉树是否等价 (So Easy)

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 func main() {
 	if true {
-		root := buildBinaryTree("[1,3,null,null,2]")
-		printBinaryTree(root)
-		recoverTree(root)
-		printBinaryTree(root)
+		t1 := buildBinaryTree("[1,2,3]")
+		t2 := buildBinaryTree("[1,2,3]")
+		println(isSameTree(t1, t2))
+	}
+	if true {
+		t1 := buildBinaryTree(" [1,2]")
+		t2 := buildBinaryTree(" [1,null,2]")
+		println(isSameTree(t1, t2))
+	}
+	if true {
+		t1 := buildBinaryTree("    [1,2,1]")
+		t2 := buildBinaryTree("[1,1,2]")
+		println(isSameTree(t1, t2))
 	}
 }
