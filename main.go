0101a004273e65b824c1d53683dedfe9a1018383
fondoger <fondoger@outlook.com>
2019-06-25 14:52:02 +0800

    #104 计算二叉树的高度（So Easy）

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 func main() {
 	if true {
 		t1 := buildBinaryTree("[3,9,20,null,null,15,7]")
-		for _, row := range zigzagLevelOrder(t1) {
-			printIntArray(row)
-		}
+		println(maxDepth(t1))
 	}
 }
