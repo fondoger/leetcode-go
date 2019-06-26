b93136b0f5f1c097ed2602a5193b5f30a6f47057
fondoger <fondoger@outlook.com>
2019-06-26 13:33:39 +0800

    #112 二叉树的路径累加和 (So Easy)

---
+++ 20190626-112.path-sum.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+ * @lc app=leetcode id=112 lang=golang
+ *
+ * [112] Path Sum
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		return root.Val == sum
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
