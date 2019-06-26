082da85009d498b69c394ca0093afc308df7177f
fondoger <fondoger@outlook.com>
2019-06-26 14:27:42 +0800

    114 二叉树右线性化 (解法1: 直观递归  解法2: 巧妙递归)

--- 20190626-114.flatten-binary-tree-to-linked-list.go
+++ 20190626-114.flatten-binary-tree-to-linked-list.go
@@ -14,8 +14,40 @@ package main
  * }
  */
 
-// 关键：前序遍历
+// 这题本来可以写得非常简单，一个递归即可
+// 但是由于golang的测试程序问题，多次调用flatten函数会影响后面的结果
+// 因此我利用golang的闭包，把它改成了如下面目全非的样子
+/*java版代码
+private TreeNode prev = null;
+
+public void flatten(TreeNode root) {
+	if (root == null) return;
+	flatten(root.Rigth)
+	flatten(root.Left)
+	root.Right = prev
+	root.Left = null
+	prev = root
+}
+*/
+// Solution 2: 简单递归
 func flatten(root *TreeNode) {
+	var prev *TreeNode
+	var flat func(*TreeNode)
+	flat = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		flat(root.Right)
+		flat(root.Left)
+		root.Right = prev
+		root.Left = nil
+		prev = root
+	}
+	flat(root)
+}
+
+// Solution 1: 我自己想出来的笨办法：递归
+func flatten_MY_STUPID_SOLUTION(root *TreeNode) {
 	helper114(root)
 }
 
