08fc699f6c263ce48879b5717c506052893566fc
fondoger <fondoger@outlook.com>
2019-06-29 11:02:21 +0800

    #6 ZigZag打印字符串 (注意处理当numRows==1时的边界问题)

--- 20190629-6.zig-zag-conversion.go
+++ 20190629-6.zig-zag-conversion.go
@@ -1,30 +1,30 @@
+package main
+
 /*
  * @lc app=leetcode id=6 lang=golang
  *
  * [6] ZigZag Conversion
  */
-package main
-
 func convert(s string, numRows int) string {
 	if numRows == 1 {
 		return s
 	}
 	strs := make([]string, numRows)
 	row, step := 0, 1
-	for _, c := range s {
-		strs[row] += string(c)
+	for i := 0; i < len(s); i++ {
+		strs[row] += string(s[i])
 		row += step
-		if row == numRows {
-			row = numRows - 2
+		if row >= numRows {
+			row -= 2
 			step = -1
-		} else if row == -1 {
+		} else if row < 0 {
 			row = 1
 			step = 1
 		}
 	}
 	res := ""
-	for _, str := range strs {
-		res += str
+	for _, s := range strs {
+		res = res + s
 	}
 	return res
 }
