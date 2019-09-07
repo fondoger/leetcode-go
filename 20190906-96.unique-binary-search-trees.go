155780e494f2bba1c5c9640299d021dd0ac34cb4
fondoger <fondoger@outlook.com>
2019-09-07 14:22:06 +0800

    #96 [tree/DP]求包含1...n个元素的BST种类数目(简单递归或者DP)

--- 20190906-96.unique-binary-search-trees.go
+++ 20190906-96.unique-binary-search-trees.go
@@ -5,7 +5,24 @@ package main
  *
  * [96] Unique Binary Search Trees
  */
+
+// 解法2: DP动态规划方式
+// 考虑解法1中的递归解，helper(min, max)看作为numTrees(max-min+1)
+// dp[2] = dp[0]*dp[1] + dp[1]*dp[0] = 2
+// dp[3] = dp[0]*dp[2] + dp[1]*dp[1] + dp[2]*dp[0] = 1*2+1*1+2*1=5
 func numTrees(n int) int {
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j-1] * dp[i-j]
+		}
+	}
+	return dp[n]
+}
+
+// 解法1: 递归解
+func numTrees_Recursively(n int) int {
 	return helper(1, n)
 }
 
