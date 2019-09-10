81263edeeeb01e65bb040d1067a4b6647698e708
fondoger <fondoger@outlook.com>
2019-09-10 23:21:04 +0800

    #104 [tree/Easy]求二叉树的高度

---
+++ 20190910-104.maximum-depth-of-binary-tree.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+ * @lc app=leetcode id=104 lang=golang
+ *
+ * [104] Maximum Depth of Binary Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+}
+
+func max(a, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
