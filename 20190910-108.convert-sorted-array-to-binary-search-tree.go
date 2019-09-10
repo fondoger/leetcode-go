f5c4c01e417418512c97d3d635ef64b45c760ca9
fondoger <fondoger@outlook.com>
2019-09-11 00:15:10 +0800

    #108 [tree/Easy]由有序数组建立平衡二叉树

---
+++ 20190910-108.convert-sorted-array-to-binary-search-tree.go
@@ -0,0 +1,26 @@
+package main
+
+/*
+ * @lc app=leetcode id=108 lang=golang
+ *
+ * [108] Convert Sorted Array to Binary Search Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 1 2 3 4
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	mid := len(nums) / 2
+	root := &TreeNode{nums[mid], nil, nil}
+	root.Left = sortedArrayToBST(nums[:mid])
+	root.Right = sortedArrayToBST(nums[mid+1:])
+	return root
+}
