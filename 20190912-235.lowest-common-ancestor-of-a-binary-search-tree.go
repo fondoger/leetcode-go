5bdc27dc1e60feedbf0c3b3c8dd3cba9f42769b6
fondoger <fondoger@outlook.com>
2019-09-13 01:00:11 +0800

    #235 [tree/Easy]找到BST中两个结点的最近公共祖先

---
+++ 20190912-235.lowest-common-ancestor-of-a-binary-search-tree.go
@@ -0,0 +1,24 @@
+package main
+
+/*
+ * @lc app=leetcode id=235 lang=golang
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ */
+/**
+ * Definition for TreeNode.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *ListNode
+ *     Right *ListNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+	return root
+}
